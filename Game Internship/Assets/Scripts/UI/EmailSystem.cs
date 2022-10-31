62035c13255bd88bbf8ff4002d62351e958539a3
KChenGitHub <chen262@canton.edu>
2022-10-31 12:43:45 -0400

    email base system

--- Game Internship/Assets/Scripts/UI/EmailSystem.cs
+++ Game Internship/Assets/Scripts/UI/EmailSystem.cs
@@ -6,10 +6,18 @@ using UnityEngine.UI;
 public class EmailSystem : MonoBehaviour
 {
     public Email currentEmail;
+    [Header("UI")]
+    public GameObject emailPanel;
+    public Text buttonText;
     [Header("Email Parts")]
     public Text subject;
     public Text sender;
     public Text body;
+
+    private void Start()
+    {
+        ShowHide();
+    }
 
     public void ChangeEmail(Email email)
     {
@@ -24,5 +32,11 @@ public class EmailSystem : MonoBehaviour
         {
             ChangeEmail(currentEmail);
         }
+    }
+
+    public void ShowHide()
+    {
+        emailPanel.SetActive(!emailPanel.activeSelf); //switch the active state
+        buttonText.text = emailPanel.activeSelf ? "Hide Email" : "Show Email";
     }
 }
