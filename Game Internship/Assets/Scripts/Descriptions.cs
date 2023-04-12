4ebf77fd1243f8cadb7feb23306f208a8f5d1f47
dido909 <steve.pappacena@gmail.com>
2023-04-11 21:47:11 -0400

    WIKI LIST COMPLETED
    WIKI HAS ALL INFORMATION NOW

--- Game Internship/Assets/Scripts/Descriptions.cs
+++ Game Internship/Assets/Scripts/Descriptions.cs
@@ -5,13 +5,33 @@ using UnityEngine;
 public class Descriptions : MonoBehaviour
 {
     public GameObject Source;
+    public GameObject Content;
+    public GameObject ExitBTN;
     public void ShowDescription()
     {
         if (Source != null)
         {
             bool isActive = Source.activeSelf;
             Source.SetActive(!isActive);
+            Debug.Log(isActive);
+            ExitBTN.SetActive(true);
+            Content.SetActive(false);
 
         }
+    }
+
+    public void GetNameList()
+    {
+        if(Source != null)
+        {
+            bool isActive = Source.activeSelf;
+            Source.SetActive(!isActive);
+            Debug.Log(isActive);
+        }
+        Content.SetActive(true);
+        ExitBTN.SetActive(false);
+
+        
+        
     }
 }
