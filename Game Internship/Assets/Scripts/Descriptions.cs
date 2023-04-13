8b8ff7556b019c8aaa1e36e93a76434a99ad2bca
dido909 <steve.pappacena@gmail.com>
2023-04-13 14:49:53 -0400

    WIKI SOURCES
    Sources match with each button

--- Game Internship/Assets/Scripts/Descriptions.cs
+++ Game Internship/Assets/Scripts/Descriptions.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class Descriptions : MonoBehaviour
 {
-    public GameObject Source;
+    public GameObject Source, Source1, Source2, Source3, Source4,
+                      Source5, Source6, Source7, Source8, Source9,
+                      Source10, Source11, Source12, Source13, Source14,
+                      Source15, Source16, Source17, Source18, Source19,
+                      Source20, Source21, Source22, Source23, Source24,
+                      Source25, Source26, Source27, Source28, Source29,
+                      Source30, Source31, Source32, Source33, Source34, Source35;
     public GameObject Content;
     public GameObject ExitBTN;
 
     void Start()
     {
-        Content.SetActive(false);
-        Source.SetActive(false);
-        ExitBTN.SetActive(false);
+        HIDE();
     }
+    //methods for getting the descriptions that each source is tied to
+    #region
+    
     public void ShowDescription()
     {
         if (Source != null)
@@ -26,17 +33,483 @@ public class Descriptions : MonoBehaviour
 
         }
     }
+    public void ShowDescription1()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source1.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription2()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source2.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription3()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source3.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription4()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source4.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription5()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source5.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription6()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source6.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription7()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source7.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription8()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source8.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription9()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source9.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription10()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source10.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription11()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source11.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription12()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source12.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription13()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source13.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription14()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source14.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription15()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source15.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription16()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source16.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription17()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source17.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription18()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source18.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription19()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source19.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription20()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source20.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription21()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source21.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription22()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source22.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription23()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source23.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription24()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source24.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription25()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source25.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription26()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source26.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription27()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source27.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription28()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source28.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription29()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source29.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription30()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source30.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription31()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source31.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription32()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source32.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription33()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source33.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription34()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source34.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+    public void ShowDescription35()
+    {
+        if (Source1 != null)
+        {
+            bool isActive = Source1.activeSelf;
+            Source35.SetActive(!isActive);
+            Debug.Log(isActive);
+            Content.SetActive(false);
+            ExitBTN.SetActive(true);
+
+        }
+    }
+
+    #endregion
+
 
     public void GetNameList()
     {
-        if(Source != null)
-        {
-            bool isActive = Source.activeSelf;
-            Source.SetActive(!isActive);
-            Debug.Log(isActive);
-        }
-        Content.SetActive(true);
+        //if(Source != null)
+        //{
+        //    bool isActive = Source.activeSelf;
+        //    Source.SetActive(!isActive);
+        //    Debug.Log(isActive);
+        //}
+
+        
         ExitBTN.SetActive(false);
+        Source.SetActive(false);
+        Source1.SetActive(false);
+        Source2.SetActive(false);
+        Source3.SetActive(false);
+        Source4.SetActive(false);
+        Source5.SetActive(false);
+        Source6.SetActive(false);
+        Source7.SetActive(false);
+        Source8.SetActive(false);
+        Source9.SetActive(false);
+
+        Source10.SetActive(false);
+        Source11.SetActive(false);
+        Source12.SetActive(false);
+        Source13.SetActive(false);
+        Source14.SetActive(false);
+        Source15.SetActive(false);
+        Source16.SetActive(false);
+        Source17.SetActive(false);
+        Source18.SetActive(false);
+        Source19.SetActive(false);
+
+        Source20.SetActive(false);
+        Source21.SetActive(false);
+        Source22.SetActive(false);
+        Source23.SetActive(false);
+        Source24.SetActive(false);
+        Source25.SetActive(false);
+        Source26.SetActive(false);
+        Source27.SetActive(false);
+        Source28.SetActive(false);
+        Source29.SetActive(false);
+
+        Source30.SetActive(false);
+        Source31.SetActive(false);
+        Source32.SetActive(false);
+        Source33.SetActive(false);
+        Source34.SetActive(false);
+        Source35.SetActive(false);
+
+
+        Content.SetActive(true);
     }
 
     public void showWiki()
@@ -45,5 +518,51 @@ public class Descriptions : MonoBehaviour
         Content.SetActive(!isActive);
 
 
+    }
+
+    public void HIDE()
+    {
+        Content.SetActive(false);
+
+        Source.SetActive(false);
+        Source1.SetActive(false);
+        Source2.SetActive(false);
+        Source3.SetActive(false);
+        Source4.SetActive(false);
+        Source5.SetActive(false);
+        Source6.SetActive(false);
+        Source7.SetActive(false);
+        Source8.SetActive(false);
+        Source9.SetActive(false);
+
+        Source10.SetActive(false);
+        Source11.SetActive(false);
+        Source12.SetActive(false);
+        Source13.SetActive(false);
+        Source14.SetActive(false);
+        Source15.SetActive(false);
+        Source16.SetActive(false);
+        Source17.SetActive(false);
+        Source18.SetActive(false);
+        Source19.SetActive(false);
+
+        Source20.SetActive(false);
+        Source21.SetActive(false);
+        Source22.SetActive(false);
+        Source23.SetActive(false);
+        Source24.SetActive(false);
+        Source25.SetActive(false);
+        Source26.SetActive(false);
+        Source27.SetActive(false);
+        Source28.SetActive(false);
+        Source29.SetActive(false);
+
+        Source30.SetActive(false);
+        Source31.SetActive(false);
+        Source32.SetActive(false);
+        Source33.SetActive(false);
+        Source34.SetActive(false);
+        Source35.SetActive(false);
+        ExitBTN.SetActive(false);
     }
 }
